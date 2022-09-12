3c76d629b27d89e53561dbc0f4ac22bd6bce5ac5
Rafael Benato <benato@email.com>
2022-09-12 15:42:18 +0100

    first commit

--- index.js
+++ index.js
@@ -7,10 +7,19 @@ const btnline3 = document.querySelector(`.btn-line3`);
 const btnlinex = document.querySelector(`.btn-linex`);
 const menu = document.querySelector(`.menu`);
 
+const btnAddition = document.querySelector(`.btnAddition`);
+const btnAdditionSub = document.querySelectorAll(`.btnAdditionsub`);
+
 menubtn.addEventListener("click", () => {
   btnline1.classList.toggle(`open`);
   btnline2.classList.toggle(`open`);
   btnline3.classList.toggle(`open`);
   btnlinex.classList.toggle(`open`);
   menu.classList.toggle(`open`);
+});
+
+btnAddition.addEventListener("click", () => {
+  btnAdditionSub.forEach((sub) => {
+    sub.classList.toggle(`hidden`);
+  });
 });
